7ceafb01005b5f9d6998124694313393809ca9d5
DESKTOP-4ORA8M1\pirik <pirikaraokaki.77@gmail.com>
2021-02-18 03:03:26 +0900

    change

---
+++ ham.js
@@ -0,0 +1,5 @@
+document.getElementById("home").onclick = function () {
+  document.getElementById("home").innerHTML = "まだ工事中です";
+};
+const news = " <a>news</a>";
+document.getElementById("news").innerHTML = "News";
